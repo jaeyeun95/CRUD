4bf94768964de1e05376047bab6f535ddce61457
jaeyeun95 <jaeyeun95@gmail.com>
2022-02-13 15:18:50 +0900

    crud 프로젝트 생성

---
+++ src/main/java/com/jy/crud/config/SpringConfig.java
@@ -0,0 +1,8 @@
+package com.jy.crud.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+    // db, 빈 의존관계 주입 설정 할 파일
+}
---
+++ src/main/java/com/jy/crud/config/WebSecurityConfig.java
@@ -0,0 +1,61 @@
+package com.jy.crud.config;
+
+import com.jy.jobweb.security.JwtAuthenticationFilter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.filter.CorsFilter;
+
+@EnableWebSecurity
+@Slf4j
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public WebSecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+//        web.ignoring().antMatchers("/static/css/**, /static/js/**, *.ico");
+        // swagger
+        web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui",
+                "/swagger-resources", "/configuration/security", "/swagger-ui.html",
+                "/webjars/**", "/swagger/**", "/h2-console/**");
+
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // http 시큐리티 빌더
+        http.cors() // WebMvcConfig에서 이미 설정했으므로 기본 cors 설정.
+                .and()
+                .csrf()// csrf는 현재 사용하지 않으므로 disable
+                .ignoringAntMatchers("/h2-console/**")
+                .disable()
+                .httpBasic()// token을 사용하므로 basic 인증 disable
+                .disable()
+                .sessionManagement()  // session 기반이 아님
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests() // /와 /user/** 경로는 인증 안해도 됨.
+                .antMatchers("/", "/user/**").permitAll()
+                .antMatchers("/swagger-ui/**", "/swagger-resources/**").permitAll()   // swagger
+                .antMatchers("/h2-console/**").permitAll() //h2-console
+                .anyRequest() // /와 /auth/**이외의 모든 경로는 인증 해야됨.
+                .authenticated();
+
+        // filter 등록.
+        // 매 리퀘스트마다
+        // CorsFilter 실행한 후에
+        // jwtAuthenticationFilter 실행한다.
+        http.addFilterAfter(
+                jwtAuthenticationFilter,
+                CorsFilter.class
+        );
+    }
+}
